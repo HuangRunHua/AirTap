75c63abf02cafd4c05d3b9f462455bd8073cfad0
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2026-03-19 13:35:02 +0800

    Initial Commit

---
+++ RemoteControl/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RemoteControl
+//
+//  Created by Runhua Huang on 2026/3/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RemoteControl/RemoteControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoteControlApp.swift
+//  RemoteControl
+//
+//  Created by Runhua Huang on 2026/3/19.
+//
+
+import SwiftUI
+
+@main
+struct RemoteControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
